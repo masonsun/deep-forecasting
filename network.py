23da51d620617da0671f4da35eb056fb6c5fe026
Mason Sun <zhengmsun@gmail.com>
2017-11-30 16:37:02 -0500

    Refactor prediction network training

--- network.py
+++ network.py
@@ -1,3 +1,4 @@
+import sys
 import argparse
 import numpy as np
 import pandas as pd
@@ -25,108 +26,53 @@ def data_validation(data, min_len):
             raise RuntimeError("Not enough timestamps at id = {}".format(g))
 
 
-def create_batch(time_series, in_dim, out_dim):
-    ts = np.array([t[1] for t in time_series], dtype=int)
-    iters = len(ts) - (in_dim + out_dim) + 1
-
-    x = [None] * iters
-    y = [None] * iters
-    for i in range(iters):
-        x[i] = ts[i: i + in_dim]
-        y[i] = ts[i + in_dim: i + in_dim + out_dim]
-    return set_dataloader(x, y)
+def save_predictions(val, filename):
+    df = pd.DataFrame(val).transpose().reset_index()
+    print(df)
+    df.columns = ['id'] + ['t+{}'.format(t + 1) for t in range(df.shape[1] - 1)]
+    print("... Saving to {}".format(filename))
+    df.to_csv('./{}'.format(filename), header=True, index=False)
 
 
-def train_nn(train_data, test_data, in_dim, out_dim, verbose=False):
-    # losses
-    train_losses = []
-    test_losses = []
+def create_batch(data, in_dim, out_dim, b_size):
+    iters = len(data[0][1]) - (in_dim + out_dim) + 1
+    x, y = [], []
 
-    # globalize
-    global model
-    global optimizer
-    global criterion
+    for game_id, time_series in data:
+        ts = np.array([t[1] for t in time_series], dtype=int)
+        for i in range(iters):
+            x_dim = ts[i: i + in_dim]
+            y_dim = ts[i + in_dim: i + in_dim + out_dim]
+            assert len(x_dim) == in_dim
+            assert len(y_dim) == out_dim
+            x.append(x_dim)
+            y.append(y_dim)
 
-    # training
-    print("... Training")
-    c = 1
-    for game_id, time_series in train_data:
-        # prepare data
-        train_loader = create_batch(time_series, in_dim, out_dim)
-
-        # train over each mini-batch "x" returned by the data loader
-        running_loss = 0
-        for i, (x, y) in enumerate(train_loader, 0):
-            # process inputs
-            if opts['use_cuda']:
-                x, y = x.cuda(), y.cuda()
-            x, y = Variable(x), Variable(y)
-
-            optimizer.zero_grad()
-
-            output = model(x)
-            loss = criterion(output, y)
-            loss.backward()
-            torch.nn.utils.clip_grad_norm(model.parameters(), opts['grad_clip'])
-            optimizer.step()
-
-            running_loss += loss.data[0]
-
-        if verbose:
-            gid = '{}...'.format(game_id[:5]) if len(game_id) > 5 else game_id
-            print('[Game {}, {:03d}/{:03d}], current training loss: {:.3f}'.format(
-                gid, c, len(data), running_loss))
-            c += 1
-
-        # training diagnostics
-        train_losses.append(running_loss / len(train_loader.dataset))
-
-    # testing
-    print("... Testing")
-    c = 1
-    for game_id, time_series in test_data:
-        # prepare data
-        test_loader = create_batch(time_series, in_dim, out_dim)
-
-        # test over mini-batch
-        running_loss = 0
-        for i, (x, y) in enumerate(test_loader, 0):
-            # process inputs
-            if opts['use_cuda']:
-                x, y = x.cuda(), y.cuda()
-            x, y = Variable(x), Variable(y)
-
-            output = model(x)
-            loss = criterion(output, y)
-            running_loss += loss.data[0]
-
-        if verbose:
-            gid = '{}...'.format(game_id[:5]) if len(game_id) > 5 else game_id
-            print('[Game {}, {:03d}/{:03d}], current testing loss: {:.3f}'.format(
-                gid, c, len(data), running_loss))
-            c += 1
-
-        # testing diagnostics
-        test_losses.append(running_loss / len(test_loader.dataset))
-
-    return np.array(train_losses).mean(axis=0), np.array(test_losses).mean(axis=0)
+    assert iters * len(data) == len(x) == len(y), 'Length mismatch after preparing time series'
+    print('... Number of observations: {}'.format(len(x)))
+    return set_dataloader(x, y, b_size)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Train prediction network")
     parser.add_argument('-f', '--filename', type=str, help='data')
     parser.add_argument('-w', '--weights', default=None, help='load trained weights')
-    parser.add_argument('-v', '--verbose', default=False, help='verbose mode')
-    parser.add_argument('-e', '--epochs', type=int, default=100, help='epochs')
-    parser.add_argument('-o', '--output_dim', type=int, default=1, help='number of days to predict')
     parser.add_argument('-m', '--mode', type=int, default=1, help='training: 0, prediction: 1')
+    parser.add_argument('-i', '--input_dim', type=int, default=180, help='input dimension')
+    parser.add_argument('-o', '--output_dim', type=int, default=60, help='output dimension')
+    parser.add_argument('-b', '--batch_size', type=int, default=1, help='batch size')
+    parser.add_argument('-e', '--epochs', type=int, default=100, help='epochs')
+    parser.add_argument('-v', '--verbose', default=False, help='verbose mode')
     args = parser.parse_args()
+
+    # dimensions
+    in_dim, out_dim = args.input_dim, args.output_dim
 
     # data
     assert args.filename is not None, 'Please provide a data file'
     data = load_data(args.filename, verbose=args.verbose)
     data = list(data.items())
-    data_validation(data, opts['frame'] + args.output_dim)
+    data_validation(data, in_dim + out_dim)
 
     # mode
     to_train = False
@@ -142,58 +88,116 @@ if __name__ == '__main__':
     if args.weights is not None and args.mode != 0:
         states = torch.load(args.weights)
         try:
-            if int(states['output_dim']) != args.output_dim:
+            if int(states['output_dim']) != out_dim:
                 raise RuntimeError("Output dim = {}, but was trained with dim = {}.".format(
-                    args.output_dim, int(states['output_dim'])))
+                    out_dim, int(states['output_dim'])))
         except KeyError:
             raise RuntimeError("Incorrect weights loaded.")
 
     # network
-    model = Predictor(model_path=args.weights, output_dim=args.output_dim)
+    model = Predictor(input_dim=in_dim, output_dim=out_dim, model_path=args.weights)
     if opts['use_cuda']:
         model = model.cuda()
 
     # training mode
     if to_train:
-        print("Training network with time frames: {} -> {}".format(opts['frame'], args.output_dim))
+        for param in model.parameters():
+            param.requires_grad = True
+
+        print("... Training network with time frames: {} -> {}".format(in_dim, out_dim))
         with open('./models/nn_log.txt', 'a') as f:
             f.write('=== New run ===\n')
 
-        # partition into training and testing sets by a 9:1 split
-        split = np.ceil(len(data) * 0.9).astype(int)
+        # partition into training and validation sets
+        split = np.ceil(len(data) * opts['train_per']).astype(int)
         train_data = data[:split]
-        test_data = data[split:]
+        valid_data = data[split:]
+
+        # convert to data loader
+        train_loader = create_batch(train_data, in_dim, out_dim, args.batch_size)
+        valid_loader = create_batch(valid_data, in_dim, out_dim, args.batch_size)
 
         # loss and optimizer
-        best_loss = np.inf
         criterion = nn.MSELoss()
-        optimizer = optim.Adam(model.parameters(), lr=opts['lr'], betas=opts['beta'], eps=opts['fudge'],
-                               weight_decay=opts['w_decay'])
+        optimizer = optim.Adam(model.parameters(), lr=opts['lr'], betas=opts['beta'],
+                               eps=opts['fudge'], weight_decay=opts['w_decay'])
 
         # history
-        train_loss = [None] * args.epochs
-        test_loss = [None] * args.epochs
+        train_losses = []
+        valid_losses = []
 
         # loop over data multiple times
-        for epoch in range(args.epochs):
+        best_loss = np.inf
+        for e in range(args.epochs):
             start_time = dt.now()
 
-            # train and test
-            train_loss[epoch], test_loss[epoch] = train_nn(train_data=train_data, test_data=test_data,
-                                                           in_dim=opts['frame'], out_dim=args.output_dim,
-                                                           verbose=args.verbose)
+            if args.verbose:
+                print("... Training")
+
+            # train over each mini-batch "x" returned by the data loader
+            model.train()
+            running_loss = 0.0
+            for i, (x, y) in enumerate(train_loader, 0):
+                # process inputs
+                if opts['use_cuda']:
+                    x, y = x.cuda(), y.cuda()
+                x, y = Variable(x), Variable(y)
+
+                optimizer.zero_grad()
+
+                output = model(x)
+                loss = criterion(output, y)
+                loss.backward()
+                torch.nn.utils.clip_grad_norm(model.parameters(), opts['grad_clip'])
+                optimizer.step()
+
+                running_loss += loss.data[0]
+
+                if args.verbose and i % 5e2 == 0:
+                    print(">>> [{:03d}%] current training loss: {:.3f}".format(
+                        np.round(100 * (i + 1) * args.batch_size / len(train_loader.dataset)).astype(int),
+                        running_loss))
+
+            # training diagnostics
+            train_losses.append(running_loss / len(train_loader.dataset))
+
+            if args.verbose:
+                print("... Testing")
+
+            # test over mini-batch
+            model.eval()
+            running_loss = 0
+            for i, (x, y) in enumerate(valid_loader, 0):
+                # process inputs
+                if opts['use_cuda']:
+                    x, y = x.cuda(), y.cuda()
+                x, y = Variable(x), Variable(y)
+
+                output = model(x)
+                loss = criterion(output, y)
+                running_loss += loss.data[0]
+
+                if args.verbose and i % 5e2 == 0:
+                    print(">>> [{:03d}%] current validation loss: {:.3f}".format(
+                        np.round(100 * (i + 1) * args.batch_size / len(valid_loader.dataset)).astype(int),
+                        running_loss))
+
+            # testing diagnostics
+            valid_losses.append(running_loss / len(valid_loader.dataset))
 
             # logging
-            log = '[Epoch {:03d}/{:03d}] Training loss: {:.3f}, Testing loss: {:.3f}, Time: {:.1f}'.format(
-                epoch + 1, args.epochs, train_loss[epoch], test_loss[epoch],
+            log = '[Epoch {:03d}/{:03d}] train loss: {:.3f}, val loss: {:.3f}, time: {:.1f}'.format(
+                e + 1, args.epochs,
+                train_losses[-1], valid_losses[-1],
                 (dt.now() - start_time).total_seconds() / 60)
+            print('>>> {}'.format(log))
 
             with open('./models/nn_log.txt', 'a') as f:
                 f.write(log + '\n')
 
             # checkpoint
             is_best = False
-            curr_loss = np.array(test_loss[:epoch + 1]).mean(axis=0)
+            curr_loss = np.array(valid_losses).mean(axis=0)
 
             if curr_loss < best_loss:
                 best_loss = curr_loss
@@ -202,46 +206,49 @@ if __name__ == '__main__':
             if opts['use_cuda']:
                 model = model.cpu()
 
-            states = {'epoch': epoch + 1,
+            states = {'epoch': e + 1,
                       'state_dict': model.vae.state_dict(),
                       'ff_dict': model.ff.state_dict(),
                       'loss': best_loss,
                       'optimizer': optimizer.state_dict(),
-                      'output_dim': args.output_dim}
+                      'output_dim': out_dim}
 
-            save_checkpoint(states, opts['nn_state'], is_best=is_best)
+            save_checkpoint(states, filename=opts['nn_state'], dim=out_dim, is_best=is_best)
 
             if opts['use_cuda']:
                 model = model.cuda()
 
-        # plot losses over epochs
-        mse_plot(np.array(train_loss), np.array(test_loss))
+        assert args.epochs == len(train_losses) == len(valid_losses), 'Mismatch across epochs'
 
-        print("Finished training and testing.")
+        # plot losses across epochs
+        mse_plot(np.array(train_losses), np.array(valid_losses))
+        print("... Finished training")
+        sys.exit(0)
 
     # prediction mode
     else:
-        print("Prediction with time frames: {} -> {}".format(opts['frame'], args.output_dim))
-
-        results = {}
+        print("... Prediction with time frames: {} -> {}".format(in_dim, out_dim))
+        result, actual = {}, {}
         for game_id, time_series in data:
-            ts = torch.from_numpy(np.array([t[1] for t in time_series], dtype=int)[-opts['frame']:]).float()
-            ts = ts.cuda() if opts['use_cuda'] else ts
+            arr = np.array([t[1] for t in time_series], dtype=int)
+            x = torch.from_numpy(arr[-(in_dim + out_dim): -out_dim]).float()
+            y = arr[-out_dim:].astype(int)
 
-            next_ts = model(Variable(ts).unsqueeze(0))
-            next_ts = next_ts.cpu() if opts['use_cuda'] else next_ts
+            x = x.cuda() if opts['use_cuda'] else x
+            y_pred = model(Variable(x).unsqueeze(0))
+            y_pred = y_pred.cpu() if opts['use_cuda'] else y_pred
 
-            results[game_id] = np.round(next_ts.data.numpy().astype(float), 1)[0]
+            result[game_id] = y_pred.data.numpy().astype(int)[0]
+            actual[game_id] = y
 
             if args.verbose:
                 gid = '{}...'.format(game_id[:5]) if len(game_id) > 5 else game_id
-                res = ', '.join(map(str, list(results[game_id])))
-                print("Game {} -> {}".format(gid, res))
+                res = ', '.join(map(str, list(result[game_id])))
+                act = ', '.join(map(str, list(actual[game_id])))
+                print("Game {} --> {}\n{} --> {}".format(gid, res, ' ' * (len(gid) + 5), act))
 
-        assert len(results) == len(data), 'Length mismatch after prediction'
-        df = pd.DataFrame(results).transpose().reset_index()
-        df.columns = ['id'] + ['t+{}'.format(t + 1) for t in range(args.output_dim)]
-
-        results_filename = 'results.csv'
-        print("Saving results to {}".format(results_filename))
-        df.to_csv('./{}'.format(results_filename), header=True, index=False)
+        assert len(result) == len(data), 'Length mismatch after prediction'
+        save_predictions(result, 'results.csv')
+        save_predictions(actual, 'ground_truth.csv')
+        print("... Finished predicting")
+        sys.exit(0)
